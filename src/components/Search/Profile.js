2b13a5e15e56ee556c999f2434021f2f0ba06f82
Prasanth Ullattil <prasanth@telenordigital.com>
2019-11-22 09:25:55 +0100

    Simplify Profile component

--- src/components/Search/Profile.js
+++ src/components/Search/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Col, Row, Card, CardBody, CardHeader, Button } from 'reactstrap';
@@ -7,89 +7,70 @@ import { subscriberActions } from '../../actions/subscriber.actions';
 import Subscription from './Subscription';
 import WarningModal from '../Shared/WarningModal';
 
-class Profile extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      showWarning: false
-    };
+function Profile(props) {
+  const [showWarning, setShowWarning] = useState(false);
+  const { deleteUser, subscriptions, profile } = props;
+
+  function handleConfirmModal() {
+    deleteUser();
+    setShowWarning(false);
   }
 
-  handleCloseModal = () => {
-    this.setState({
-      showWarning: false
-    });
-  };
+  const modalHeading = `Confirm remove user`;
+  const modalText = `Do you really want to remove this user?`;
 
-  handleShowModal = () => {
-    this.setState({
-      showWarning: true
-    });
-  };
-
-  handleConfirmModal = () => {
-    this.props.deleteUser();
-    this.handleCloseModal();
-  };
-
-  render() {
-    const modalHeading = `Confirm remove user`;
-    const modalText = `Do you really want to remove this user?`;
-    const props = this.props;
-
-    let listItems = null;
-    if (Array.isArray(props.subscriptions.items)) {
-      listItems = props.subscriptions.items.map((subscription, index) => (
-        <div key={index}>
-          <Subscription subscription={subscription} key={index} />
-          <hr />
-        </div>
-      ));
-    }
-    return (
-      <Card>
-        <CardHeader>Customer</CardHeader>
-        <CardBody>
-          <Row>
-            <Col xs={2} md={2}>
-              {'Name:'}
-            </Col>
-            <Col xs={12} md={8}>{`${props.profile.nickname}`}</Col>
-          </Row>
-          <Row>
-            <Col xs={2} md={2}>
-              {'Email:'}
-            </Col>
-            <Col xs={12} md={8}>{`${props.profile.contactEmail}`}</Col>
-          </Row>
-          <Row>
-            <Col xs={2} md={2}>
-              {'ID:'}
-            </Col>
-            <Col xs={12} md={8}>{`${props.profile.id}`}</Col>
-          </Row>
-          <br />
-          <Row>
-            <Col xs={6} md={4}>
-              <Button color="danger" onClick={this.handleShowModal}>
-                {'Remove User'}
-              </Button>
-            </Col>
-          </Row>
-          <hr />
-          {listItems}
-          <WarningModal
-            heading={modalHeading}
-            dangerStyle={true}
-            warningText={modalText}
-            show={this.state.showWarning}
-            handleConfirm={this.handleConfirmModal}
-            handleClose={this.handleCloseModal}
-          />
-        </CardBody>
-      </Card>
-    );
+  let listItems = null;
+  if (Array.isArray(subscriptions.items)) {
+    listItems = subscriptions.items.map((subscription, index) => (
+      <div key={index}>
+        <Subscription subscription={subscription} key={index} />
+        <hr />
+      </div>
+    ));
   }
+  return (
+    <Card>
+      <CardHeader>Customer</CardHeader>
+      <CardBody>
+        <Row>
+          <Col xs={2} md={2}>
+            {'Name:'}
+          </Col>
+          <Col xs={12} md={8}>{`${profile.nickname}`}</Col>
+        </Row>
+        <Row>
+          <Col xs={2} md={2}>
+            {'Email:'}
+          </Col>
+          <Col xs={12} md={8}>{`${profile.contactEmail}`}</Col>
+        </Row>
+        <Row>
+          <Col xs={2} md={2}>
+            {'ID:'}
+          </Col>
+          <Col xs={12} md={8}>{`${props.profile.id}`}</Col>
+        </Row>
+        <br />
+        <Row>
+          <Col xs={6} md={4}>
+            <Button color="danger" onClick={() => setShowWarning(true)}>
+              {'Remove User'}
+            </Button>
+          </Col>
+        </Row>
+        <hr />
+        {listItems}
+        <WarningModal
+          heading={modalHeading}
+          dangerStyle={true}
+          warningText={modalText}
+          show={showWarning}
+          handleConfirm={handleConfirmModal}
+          handleClose={() => setShowWarning(false)}
+        />
+      </CardBody>
+    </Card>
+  );
 }
 
 Profile.propTypes = {
